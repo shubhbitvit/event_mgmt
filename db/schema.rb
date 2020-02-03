819e51c982f01493536520db2bd91b47dbf77e54
shubhbitvit <subhams914@gmail.com>
2020-02-03 12:53:45 +0530

    Rename typein event

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200202110558) do
+ActiveRecord::Schema.define(version: 20200203070121) do
 
   create_table "events", force: :cascade do |t|
     t.date "date"
-    t.string "type"
+    t.string "event_type"
     t.integer "cost"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
